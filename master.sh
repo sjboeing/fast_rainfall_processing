6bf0102ec4965ac4c606cc5452ea85ec6c2efc22
Ben Maybee <bmaybee@sci1.jasmin.ac.uk>
2024-05-07 16:05:41 +0100

    Minor corrections to master shell scripts; improved commenting of workflow

--- master.sh
+++ master.sh
@@ -4,15 +4,19 @@
 date_info=$(date --date=$1 +"%Y%m%d")
 fcst_init=$2
 
+# NOTE: running rainfall neighbourhood-processing algorithm (integrated_rainfall_processing.py) relies on numba python pacakge. Some routines also use multiprocessing parallel computation for speed, although this is not especially computationally expensive.
+
+#################################################################
 #KEY DIRECTORIES - CHANGE TO USER NEEDS
 gws_dir=/gws/nopw/j04/icasp_swf/bmaybee
-user_dir=/home/users/bmaybee
+user_dir=/home/users/bmaybee/iCASP/fast_rainfall_processing
+#################################################################
 
 #Retrieve and combine mogreps ensemble forecasts - requires access to MASS archive.
 . ${user_dir}/mogreps_retrieval.sh $date_info $fcst_init
 #datadir=${gws_dir}/mogreps_ensembles/${date_info:0:6}
 
-#Apply percentile processing to forecasts. Note requirement for 18 member ensemble - sets failsafe $run.
+# Note requirement for 18 member ensemble - sets failsafe parameter $run.
 # $datadir defined within previous routine. Temporary folder for mass deposit.
 if [[ $(ls $datadir/*003.pp | wc -l) == 18 ]]
 then 
@@ -33,18 +37,26 @@ fi
 
 if [[ $run == 1 ]]
 then
-  ssh -A sci6 "module load jaspy
+  # Transfer to higher memory server for processing. Essential for first routine.
+  ssh -A sci6 "
+  # Code designed for use on UK JASMIN collaborative platform. Python packages available in bulk via jaspy environments - full specifications can be found at https://help.jasmin.ac.uk/docs/software-on-jasmin/jaspy-envs/.
+  # Specification essential to ensure access to specific numba environment; relied on for post-processing algorithm.
+  module load jaspy/3.10/r20220721
+  # ESSENTIAL - neighbourhood post-processing routine applied to raw rainfall ensemble fields:
   python ${user_dir}/integrated_rainfall_processing.py -f $date_info -i ${fcst_init} -nc False -loc ${gws_dir}
+  # forecast_plots and FloodForecastLookup both require packages within an older jaspy configuration (specifically, cartopy and ogr)
+  module load jaspy/3.7
+  # OPTIONAL - forecat plots made for rainfall output only; useful for detailed investigation:
   python ${user_dir}/forecast_plots.py -f ${date_info} -i ${fcst_init} -loc ${gws_dir} -u ${user_dir}
+  # ESSENTIAL - catchment-level look up of neighbourhood-processed rainfall field values
   python ${user_dir}/extract_catchment_fcsts.py -f ${date_info} -i ${fcst_init} -reg EngWls -loc ${gws_dir}
   "
-
-  # FloodForecastLookup requires older jaspy version, therefore switch to 3.7
-  module load jaspy/3.7
-  python ${user_dir}/FloodForecastLookup.py -f ${date_info} -i ${fcst_init} -p 98 -reg EngWls -loc ${gws_dir} -u ${user_dir}
-  python ${user_dir}/present_forecasts.py -f ${date_info} -i ${fcst_init} -u ${user_dir}
   
-#  . ${user_dir}/make_fcst_html.sh ${date_info}
+  # ESSENTIAL - compare catchment-level rainfall values with reference thresholds and generate flood forecast images
+  python ${user_dir}/FloodForecastLookup.py -f ${date_info} -i ${fcst_init} -p 98 -reg EngWls -loc ${gws_dir} -u ${user_dir}
+  # OPTIONAL - build pdf compiling flood and rainfall forecast images. Danger: hangs if ANY files missing
+  #python ${user_dir}/present_forecasts.py -f ${date_info} -i ${fcst_init} -u ${user_dir}
+  
 else
   echo "No processing"
 fi
--- obs_master.sh
+++ obs_master.sh
@@ -8,16 +8,18 @@ if [[ $(date +"%Y%m%d") == $date_info ]]; then
 fi
 echo $date_info
 
+#################################################################
 #KEYY DIRECTORIES - CHANGE TO USER NEEDS
 gws_dir=/gws/nopw/j04/icasp_swf/bmaybee
-user_dir=/home/users/bmaybee
+user_dir=/home/users/bmaybee/iCASP/fast_rainfall_processing
+#################################################################
 
 #NIMROD RADAR
 . ${user_dir}/radar_retrieval.sh $date_info
-# $radardir defined within retrieval loop
-#radardir=${gws_dir}/radar_obs/$(date -d ${date_info} +"%Y")/temp
-#radardir=${gws_dir}/radar_obs/temp
+# Note: $radardir defined within retrieval loop. Uncomment if shell script not run.
+#radardir=${gws_dir}/radar_obs/$(date -d ${date_info} +"%Y")
 
+# Note requirement for single .nc file covering time period (calendar day by default) - sets failsafe parameter $run.
 if [[ $(ls $radardir/*.nc | wc -l) == 1 ]]
   then
   rm $radardir/*.tar
@@ -37,17 +39,26 @@ fi
 #   -reg : Flood forecast region; corresponds to string in name of csv tables of catchment level flood threshold values. Current options are NEng or EngWls
 
 if [[ $run == 1 ]]; then
+  # Transfer to higher memory server for processing. Essential for first routine.
   ssh -A sci6 "
-  module load jaspy
+  # Code designed for use on UK JASMIN collaborative platform. Python packages available in bulk via jaspy environments - full specifications can be found at https://help.jasmin.ac.uk/docs/software-on-jasmin/jaspy-envs/.
+  # Specification essential to ensure access to specific numba environment; relied on for post-processing algorithm.
+  module load jaspy/3.10/r20220721
+  # ESSENTIAL - neighbourhood post-processing routine applied to raw radar rainfall fields:
   python ${user_dir}/integrated_rainfall_processing.py -f ${date_info} -i 0 -r True -loc ${gws_dir}
-  python ${user_dir}/obs_plots.py -f $date_info -loc ${gws_dir} -u ${user_dir}
-  python ${user_dir}/extract_catchment_fcsts.py -f ${date_info}_00 -r True -reg EngWls -loc ${gws_dir}
-  "
-  # FloodForecastLookup requires older jaspy version, therefore switch to 3.7
+  # forecast_plots and FloodForecastLookup both require packages within an older jaspy configuration (specifically, cartopy and ogr)
   module load jaspy/3.7
+  # OPTIONAL - forecat plots made for rainfall output only; useful for detailed investigation:
+  python ${user_dir}/obs_plots.py -f $date_info -loc ${gws_dir} -u ${user_dir}
+  # ESSENTIAL - catchment-level look up of neighbourhood-processed rainfall field values
+  python ${user_dir}/extract_catchment_fcsts.py -f ${date_info} -r True -reg EngWls -loc ${gws_dir}
+  "
+
+  # ESSENTIAL - compare catchment-level rainfall values with reference thresholds and generate flood forecast images
   python ${user_dir}/FloodForecastLookup.py -f ${date_info} -r True -reg EngWls -loc ${gws_dir} -u ${user_dir}
+  # OPTIONAL - updates rainfall-based forecast plots to include radar "truth"
   # -f here is the date of the forecast initialised the day prior to observations at 18UTC. Change to meet user specifications! Only generates evaluation plots.
   python ${user_dir}/forecast_plots.py -f $(date -d "${date_info} - 1 day" +"%Y%m%d") -i 18 -e True -d $date_info -loc ${gws_dir} -u ${user_dir}
-  python ${user_dir}/present_evaluations.py -f ${date_info} -i 18 -u ${user_dir}
-  #. ${user_dir}/make_obs_html.sh $date_info
+  # OPTIONAL - build pdf compiling evaluation images. Danger: hangs if ANY files missing
+  #python ${user_dir}/present_evaluations.py -f ${date_info} -i 18 -u ${user_dir}
 fi
